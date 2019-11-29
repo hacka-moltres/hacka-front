62b65527579a92f951699f7fd88845fd46d6d546
davisbento <davisilva4222@gmail.com>
2019-11-29 15:54:50 -0300

    Fingerprint

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,8 +1,9 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import logo from 'assets/img/capa.jpeg';
 import { Paper, TextField, Grid, Button } from '@material-ui/core';
 import trackFactory from 'services/track';
+import Fingerprint2 from 'fingerprintjs2';
 
 const useStyles = makeStyles({
   container: {
@@ -29,9 +30,21 @@ trackFactory.identifyUser();
 export default memo(() => {
   const classes = useStyles({});
 
+  const callFingerprint = useCallback(async () => {
+    const components: { key: string; value: any }[] = await Fingerprint2.getPromise({});
+    const values = components.map((component: any) => component.value);
+    const hash = Fingerprint2.x64hash128(values.join(''), 31);
+    console.log(components);
+    console.log(hash);
+  }, []);
+
+  useEffect(() => {
+    callFingerprint();
+  }, [callFingerprint]);
+
   return (
     <div className={classes.container}>
-      <Grid container justify='center' alignItems='center'>
+      <Grid container spacing={4} justify='center' alignItems='center'>
         <Grid item sm={12} md={6}>
           <img src={logo} alt='logo' className={classes.image} />
         </Grid>
